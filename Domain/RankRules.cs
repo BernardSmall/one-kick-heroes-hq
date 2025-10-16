67c8afb61048823d0ed21ae73df18fb49cf19fd8
600848 <600848@student.belgiumcampus.ac.za>
2025-10-16 09:39:19 +0200

    Created Classes: -Hero -Rank -RankRules -ThreatLevel

---
+++ Domain/RankRules.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneKickHeroesApp.Domain
+{
+    public static class RankRules
+    {
+        // S: 81–100, A: 61–80, B: 41–60, C: 0–40
+        public static Rank FromScore(double s)
+        {
+            if (s >= 81) return Rank.S;
+            if (s >= 61) return Rank.A;
+            if (s >= 41) return Rank.B;
+            return Rank.C;
+        }
+
+        public static ThreatLevel ThreatFor(Rank r)
+        {
+            switch (r)
+            {
+                case Rank.S: return ThreatLevel.FinalsWeek;
+                case Rank.A: return ThreatLevel.MidtermMadness;
+                case Rank.B: return ThreatLevel.GroupProjectGoneWrong;
+                default: return ThreatLevel.PopQuiz;
+            }
+        }
+    }
+}
---
+++ Domain/ThreatLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneKickHeroesApp.Domain
+{
+    public enum ThreatLevel
+    {
+        FinalsWeek,            // S
+        MidtermMadness,        // A
+        GroupProjectGoneWrong, // B
+        PopQuiz                // C
+    }
+}
